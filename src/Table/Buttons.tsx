af785334e25ddcf3878a53b2c4aa03311d754a94
Alexander Chaschin <ac@happynumbers.com>
2024-01-09 23:09:42 +0100

    fix identation

--- src/Table/Buttons.tsx
+++ src/Table/Buttons.tsx
@@ -8,19 +8,19 @@ export function Buttons() {
 
   return (
     <View style={styles.icons}>
-    <Chip
-      testID={'copy-to-clipboard'}
-      icon={{ name: 'copy', type: 'font-awesome', color: 'white' }}
-      containerStyle={styles.bottomIcon}
-      onPress={() => actor.send({ type: "copy data" })}
-    />
-    <Chip
-      testID={'clear-data'}
-      icon={{ name: 'trash', type: 'font-awesome', color: 'white' }}
-      containerStyle={styles.bottomIcon}
-      color={'warning'}
-      onPress={() => actor.send({ type: "clear data" })}
-    />
-  </View>
+      <Chip
+        testID={'copy-to-clipboard'}
+        icon={{ name: 'copy', type: 'font-awesome', color: 'white' }}
+        containerStyle={styles.bottomIcon}
+        onPress={() => actor.send({ type: "copy data" })}
+      />
+      <Chip
+        testID={'clear-data'}
+        icon={{ name: 'trash', type: 'font-awesome', color: 'white' }}
+        containerStyle={styles.bottomIcon}
+        color={'warning'}
+        onPress={() => actor.send({ type: "clear data" })}
+      />
+    </View>
   );
 }
