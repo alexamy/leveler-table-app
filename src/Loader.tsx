98006f35b484de5b926318d5cf84e4b0e1b9d47c
Alexander Chaschin <ac@happynumbers.com>
2024-01-09 23:32:42 +0100

    add loading view

--- src/Loader.tsx
+++ src/Loader.tsx
@@ -2,11 +2,20 @@ import { levelerMachine } from './machine';
 import { useLoadSnapshot } from './persist';
 import { View } from 'react-native';
 import { Root } from './Root';
+import { Skeleton } from '@rneui/themed';
 
 export function Loader() {
   const [snapshot, isLoading] = useLoadSnapshot(levelerMachine.id);
 
   return (
-    isLoading ? <View /> : <Root snapshot={snapshot} />
+    isLoading ? <Loading /> : <Root snapshot={snapshot} />
+  );
+}
+
+function Loading() {
+  return (
+    <View>
+      <Skeleton />
+    </View>
   );
 }
