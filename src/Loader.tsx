3231d20209e69cf38cdfbd285d36e37164e3c343
Alexander Chaschin <ac@happynumbers.com>
2024-01-09 10:56:22 +0100

    add loader

--- src/Loader.tsx
+++ src/Loader.tsx
@@ -1,19 +1,12 @@
 import { levelerMachine } from './machine';
-import { Table } from './Table';
-import { MachineContext } from './MachineContext';
 import { useLoadSnapshot } from './persist';
 import { View } from 'react-native';
-import { Persistence } from './Persistence';
+import { Root } from './Root';
 
 export function Loader() {
   const [snapshot, isLoading] = useLoadSnapshot(levelerMachine.id);
 
   return (
-    isLoading ? <View /> :
-    <MachineContext.Provider options={{ snapshot }}>
-      <Persistence>
-        <Table />
-      </Persistence>
-    </MachineContext.Provider>
+    isLoading ? <View /> : <Root snapshot={snapshot} />
   );
 }
---
+++ src/Root.tsx
@@ -0,0 +1,25 @@
+import { SnapshotFrom } from 'xstate';
+import { levelerMachine } from './machine';
+import { MachineContext } from './MachineContext';
+import { Table } from './Table';
+import { useSaveSnapshot } from './persist';
+
+function Persistence({ children }: { children: JSX.Element }) {
+  const actor = MachineContext.useActorRef();
+  useSaveSnapshot(levelerMachine.id, actor);
+
+  return <>{children}</>;
+}
+
+
+export function Root({ snapshot }: {
+  snapshot: SnapshotFrom<typeof levelerMachine>,
+}) {
+  return (
+    <MachineContext.Provider options={{ snapshot }}>
+      <Persistence>
+        <Table />
+      </Persistence>
+    </MachineContext.Provider>
+  );
+}
