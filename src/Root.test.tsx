5caa3bb2ad7705b52982a39cc25c392fcc9eece5
Alexander Chaschin <ac@happynumbers.com>
2024-01-09 10:56:40 +0100

    rename root export

--- src/Root.test.tsx
+++ src/Root.test.tsx
@@ -3,7 +3,7 @@ import { act, fireEvent, render, screen, waitFor } from '@testing-library/react-
 import { TextInput } from 'react-native';
 import * as Clipboard from 'expo-clipboard';
 import { dedent } from 'ts-dedent';
-import { Table as Root } from './Table';
+import { Root } from './Root';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import App from '../App';
 
