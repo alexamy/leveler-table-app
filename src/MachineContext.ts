b6f633ace1e1afab5eb00f19078e60c3ced6cf0b
Alexander Chaschin <ac@happynumbers.com>
2024-01-09 23:41:49 +0100

    add copy toast

--- src/MachineContext.ts
+++ src/MachineContext.ts
@@ -1,11 +1,19 @@
 import * as Clipboard from 'expo-clipboard';
 import { createActorContext } from '@xstate/react';
 import { levelerMachine } from './machine';
+import { ToastAndroid } from 'react-native';
 
 const machine = levelerMachine.provide({
   actions: {
     "copy data to clipboard": (_, { table }) => {
       Clipboard.setStringAsync(table);
+    },
+    "show copy toast": () => {
+      ToastAndroid.showWithGravity(
+        "Таблица скопирована!",
+        ToastAndroid.SHORT,
+        ToastAndroid.TOP,
+      );
     },
   },
 });
