6e599400fb4932dfb71307a2b96e201f12a8dae8
Alexander Chaschin <ac@happynumbers.com>
2024-01-09 11:36:45 +0100

    rename again

--- src/Table/index.tsx
+++ src/Table/index.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 import { Buttons } from './Buttons';
-import { Measurments } from './Measurments';
+import { Measurements } from './Measurements';
 import { Zero } from './Zero';
 import { styles } from './styles';
 
@@ -9,7 +9,7 @@ export function Table() {
   return (
     <View style={styles.container}>
       <Zero />
-      <Measurments />
+      <Measurements />
       <Buttons />
       <StatusBar style='auto' />
     </View>
